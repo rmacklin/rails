8d0ebdfa4d3a06fc2151d9b8cebd5e1e114d6917
Richard Macklin <richard.github@nrm.com>
2018-10-18 12:53:49 -0700

    Replace most ActionCable.* references with finer-grained imports
    There are now 2 circular dependencies:
    
    ```
    (!) Circular dependency: app/javascript/action_cable/action_cable.js -> app/javascript/action_cable/consumer.js -> app/javascript/action_cable/connection.js -> app/javascript/action_cable/action_cable.js
    (!) Circular dependency: app/javascript/action_cable/action_cable.js -> app/javascript/action_cable/consumer.js -> app/javascript/action_cable/connection.js -> app/javascript/action_cable/connection_monitor.js -> app/javascript/action_cable/action_cable.js
    ```
    
    but we are on our way towards eliminating all of them. We'll extract
    additional modules to accomplish this.

--- actioncable/app/javascript/action_cable/action_cable.js
+++ actioncable/app/javascript/action_cable/action_cable.js
@@ -1,4 +1,4 @@
-import ActionCable from "./index"
+import Consumer from "./consumer"
 
 export default {
   WebSocket: window.WebSocket,
@@ -9,7 +9,7 @@ export default {
       const urlConfig = this.getConfig("url")
       url = (urlConfig ? urlConfig : this.INTERNAL.default_mount_path)
     }
-    return new ActionCable.Consumer(this.createWebSocketURL(url))
+    return new Consumer(this.createWebSocketURL(url))
   },
 
   getConfig(name) {
